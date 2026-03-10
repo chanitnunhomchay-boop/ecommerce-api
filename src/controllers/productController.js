97a825808242761c2dd42c3a76dbecc932760232
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 02:04:21 +0700

    Refactor productController to use caching and db

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,19 +1,23 @@
-let products = []
+const db = require("../data/db")
+const cache = require("../services/cacheService")
 
-exports.getProducts = (req, res) => {
-  res.json(products)
+exports.getProducts = (req,res)=>{
+    const cached = cache.get("products")
+    if(cached) return res.json(cached)
+
+    cache.set("products", db.products)
+
+    res.json(db.products)
 }
 
-exports.createProduct = (req, res) => {
+exports.createProduct = (req,res)=>{
+    const product = {
+        id:Date.now(),
+        name:req.body.name,
+        price:req.body.price
+    }
 
-  const product = {
-    id: Date.now(),
-    name: req.body.name,
-    price: req.body.price
-  }
+    db.products.push(product)
 
-  products.push(product)
-
-  res.status(201).json(product)
-
+    res.status(201).json(product)
 }
