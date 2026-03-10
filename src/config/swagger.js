33786bb5a5cf545a3af558e6c6c2b9be69a2ee1b
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 04:10:06 +0700

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,22 +1,36 @@
-const swaggerJsDoc = require("swagger-jsdoc")
+const swaggerJSDoc = require("swagger-jsdoc")
 
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Ecommerce API",
+      title: "E-commerce RESTful API",
       version: "1.0.0",
-      description: "REST API for Ecommerce"
+      description: "API documentation for E-commerce system"
     },
     servers: [
       {
         url: "http://localhost:3000"
       }
+    ],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT"
+        }
+      }
+    },
+    security: [
+      {
+        BearerAuth: []
+      }
     ]
   },
-  apis: ["./src/**/*.js"]
+  apis: ["./src/routes/*.js"]
 }
 
-const swaggerSpec = swaggerJsDoc(options)
+const swaggerSpec = swaggerJSDoc(options)
 
 module.exports = swaggerSpec
