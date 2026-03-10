944065dad9ebeb600eb447bfb6d16aeab364d722
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 02:02:15 +0700

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,22 +1,16 @@
 const jwt = require("jsonwebtoken")
 
-function authenticateToken(req, res, next) {
+module.exports = function(req, res, next){
+    const authHeader = req.headers.authorization
+    if(!authHeader) return res.status(401).json({message:"No token"})
 
-  const authHeader = req.headers["authorization"]
-  const token = authHeader && authHeader.split(" ")[1]
+    const token = authHeader.split(" ")[1]
 
-  if (!token) return res.sendStatus(401)
-
-  jwt.verify(token, "secretkey", (err, user) => {
-
-    if (err) return res.sendStatus(403)
-
-    req.user = user
-
-    next()
-
-  })
-
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    }catch(err){
+        res.status(401).json({message:"Invalid token"})
+    }
 }
-
-module.exports = authenticateToken
