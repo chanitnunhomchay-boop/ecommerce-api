4e454a26e53a706d03b4d54f3b9c99fb451b1fe5
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 03:08:03 +0700

    Update orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,7 +4,28 @@ const router = express.Router()
 const controller = require("../controllers/orderController")
 const auth = require("../middleware/authMiddleware")
 
-router.get("/",auth,controller.getOrders)
-router.post("/",auth,controller.createOrder)
+/**
+ * @swagger
+ * /api/v1/orders:
+ *   get:
+ *     summary: Get user orders
+ *     tags: [Orders]
+ *     responses:
+ *       200:
+ *         description: List of orders
+ */
+router.get("/", auth, controller.getOrders)
+
+/**
+ * @swagger
+ * /api/v1/orders:
+ *   post:
+ *     summary: Create new order
+ *     tags: [Orders]
+ *     responses:
+ *       201:
+ *         description: Order created
+ */
+router.post("/", auth, controller.createOrder)
 
 module.exports = router
