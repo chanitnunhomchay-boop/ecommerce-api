abb4862c79cbf2f7b839f836266d0b078d731d92
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 03:08:22 +0700

    Integrate Swagger for product route documentation
    Added Swagger documentation for product routes.

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,7 +5,48 @@ const controller = require("../controllers/productController")
 const auth = require("../middleware/authMiddleware")
 const role = require("../middleware/roleMiddleware")
 
-router.get("/",controller.getProducts)
-router.post("/",auth,role("admin"),controller.createProduct)
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: Product management
+ */
+
+/**
+ * @swagger
+ * /api/v1/products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: List of products
+ */
+router.get("/", controller.getProducts)
+
+/**
+ * @swagger
+ * /api/v1/products:
+ *   post:
+ *     summary: Create new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               stock:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Product created
+ */
+router.post("/", auth, role("admin"), controller.createProduct)
 
 module.exports = router
