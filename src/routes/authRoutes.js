4c339add27f7c591041820ac62c5dc21f403354e
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 03:07:43 +0700

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,61 @@
 const express = require("express")
 const router = express.Router()
-const auth = require("../controllers/authController")
 
-router.post("/register",auth.register)
-router.post("/login",auth.login)
+const controller = require("../controllers/authController")
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication system
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Register new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered
+ */
+router.post("/register", controller.register)
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ */
+router.post("/login", controller.login)
 
 module.exports = router
