3228cee546c463a9aa082a8d1a969190b4ca8d16
chanitnunhomchay-boop <chanitnunhomchay@gmail.com>
2026-03-11 02:06:18 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
+require("dotenv").config()
+
 const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+const swaggerUI = require("swagger-ui-express")
+const swaggerSpec = require("./config/swagger")
+
+const rateLimiter = require("./middleware/rateLimiter")
+const errorMiddleware = require("./middleware/errorMiddleware")
+
+const authRoutes = require("./routes/authRoutes")
+const productRoutes = require("./routes/productRoutes")
+const orderRoutes = require("./routes/orderRoutes")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(rateLimiter)
 
-app.get("/", (req, res) => {
-  res.json({ message: "E-commerce API running" })
-})
+app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/products",productRoutes)
+app.use("/api/v1/orders",orderRoutes)
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpec))
+
+app.use(errorMiddleware)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
 })
